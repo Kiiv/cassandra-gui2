6b9af0ee190ee909907baf175fc17e670c190bb4
honore.c@gmail.com <honore.c@gmail.com@227cc8f5-303a-2932-df8a-888710761682>
2014-01-24 15:10:14 +0000

    Initial import.

---
+++ trunk/src/main/java/org/apache/cassandra/gui/control/callback/PropertiesCallback.java
@@ -0,0 +1,9 @@
+package org.apache.cassandra.gui.control.callback;
+
+public interface PropertiesCallback {
+  public void clusterCallback();
+
+  public void keyspaceCallback(String keyspace);
+
+  public void columnFamilyCallback(String keyspace, String columnFamily);
+}
